3a86bd624184a5f2a8cea989781c39b2c7b3dbe7
ballj <julius.bal@edu.htl-villach.at>
2023-06-07 15:44:09 +0200

    c

--- php/navbar.php
+++ php/navbar.php
@@ -1,25 +1,25 @@
 
 <?php
     $navVisibility = 'style="display: none"';
-    $navStatus = FALSE;
-    
+    $openNavVisibility = '';
+    $closeNavVisibility = 'style="display: none"';
+    $backgroundStatus = 'style="background-image: none;"';
+
 
     if($_SERVER["REQUEST_METHOD"] == "POST"){
         if(isset($_POST['openNav']) == TRUE){
-            switch($navStatus) {
-                case FALSE:
-                    $navVisibility = "";
-                    $navStatus = TRUE;
-                    break;
-        
-                case TRUE:
-                    $navVisibility = 'style="display: none"';
-                    $navStatus = FALSE;
-                    break;
-            }
+            $navVisibility = '"';
+            $openNavVisibility = 'style="display: none"';
+            $closeNavVisibility = '';
+            $backgroundStatus = '';
+        }
+        if(isset($_SERVER['closeNav']) == TRUE){
+            $navVisibility = 'style="display: none';
+            $openNavVisibility = '';
+            $closeNavVisibility = 'style="display: none"';
+            $backgroundStatus = 'style="background-image: none;"';
         }
     }
-
 ?>
 
 <nav class="navbar-container">
@@ -28,34 +28,37 @@
         <a class="mail-icon" href="mailto:juliusball197@gmail.com"><img class="icon" src="img/Icons/Navbar/mail.png" alt="Loading error"></a>
     </div>
 
-
-    <div class="nav-links" <?php echo $navVisibility; ?>>
-
-        <div class="nav-link-item text-uppercase">
-            <a href="#section-myself"><p class="text-albWhite">Myself</p></a>
-        </div>
-        
-        <div class="nav-link-item text-uppercase">
-            <a href="#section-skills"><p class="text-albWhite">Skills</p></a>
-        </div>
-        
-        <div class="nav-link-item text-uppercase">
-            <a href="#section-projects"><p class="text-albWhite">Projects</p></a>
-        </div>
-        
-        <div class="nav-link-item text-uppercase">
-            <a href="#section-contact"><p class="text-albWhite">Contact</p></a>
+    <div class="nav-content-container" <?php echo $backgroundStatus; ?>>
+        <!-- <img class="mobileNav-background" src="img/Backgrounds/Nav-background.png" alt="Error loading"> -->
+        <div class="openNavIcon">
+            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+                <button <?php echo $openNavVisibility; ?> type="submit" name="openNav">
+                    <img class="icon" src="img/Icons/Navbar/open-nav-albescentwhite.png" alt="Loading Error">
+                </button>
+                <button <?php echo $closeNavVisibility; ?> type="submit" name="closeNav">
+                    <img class="icon" src="img/Icons/Navbar/open-nav-spicymix.png" alt="Loading Error">
+                </button>
+            </form>
         </div>
 
+        <div class="nav-links" <?php echo $navVisibility; ?>>
+
+            <div class="nav-link-item text-uppercase">
+                <a href="#section-myself"><p class="text-spicyMix">Myself</p></a>
+            </div>
+            
+            <div class="nav-link-item text-uppercase">
+                <a href="#section-skills"><p class="text-spicyMix">Skills</p></a>
+            </div>
+            
+            <div class="nav-link-item text-uppercase">
+                <a href="#section-projects"><p class="text-spicyMix">Projects</p></a>
+            </div>
+            
+            <div class="nav-link-item text-uppercase">
+                <a href="#section-contact"><p class="text-spicyMix">Contact</p></a>
+            </div>
+
+        </div>
     </div>
-
-    <div class="openNavIcon">
-        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
-            <button type="submit" name="openNav">
-                <img class="icon" src="img/Icons/Navbar/open-nav-albescentwhite.png" alt="Loading Error">
-            </button>
-        </form>
-    </div>
-
-
 </nav>
